d2a7dcec9dc87d2ed542ae6c25560959811bb2b4
jasur <jasurbeklar7777@gmail.com>
2022-10-18 20:53:57 -0500

    update db

--- handlers/users/reklama.py
+++ handlers/users/reklama.py
@@ -1,8 +1,9 @@
 import asyncio
-from aiogram.types import Message, ContentType
+from aiogram.types import Message, ContentType,ReplyKeyboardRemove
 from data.config import ADMINS
 from aiogram.dispatcher import FSMContext
 
+from keyboards.default.contact import true_false
 from keyboards.inline.url_english import qora
 from loader import db,dp,bot
 
@@ -78,3 +79,30 @@ async def caption_video(message:Message,state:FSMContext):
 async def resimiy_kanal(message:Message):
     id = 'AgACAgIAAxkBAAIHwGNNrDSpSsog-ruh7NReUfpbmz6FAAK2wjEbbFZoSvs8xjavhGNRAQADAgADeQADKgQ'
     await message.answer_photo(photo=id,reply_markup=qora)
+
+@dp.message_handler(text = '/all_users',user_id=ADMINS[0])
+async def all_user_def(message:Message,state:FSMContext):
+    users = db.select_all_users()
+    a = []
+    for i in users:
+        a.append(f'Ism:{i[1]}\n'
+                 f'Tel:{i[2]}')
+    for j in a:
+        await message.answer(j)
+
+@dp.message_handler(text = '/delete_users',user_id = ADMINS[0])
+async def delete_user_def(message:Message,state:FSMContext):
+    await message.answer('DIQQAT!\n'
+                         'Hamma foydalanuvchi bazadan o\'chadi!',reply_markup=true_false)
+    await state.set_state('delete_user')
+
+@dp.message_handler(state='delete_user')
+async def delete_user_state_def(message:Message,state:FSMContext):
+    if message.text=='✅ Ha':
+          db.delete_users()
+          await message.answer("Hamma Foydalanuvchi bazadan o'chirildi ✅",reply_markup=ReplyKeyboardRemove())
+          await state.finish()
+    else:
+        await message.answer('Bazada foydalanuvchilar o\'chirilmadi',reply_markup=ReplyKeyboardRemove())
+        await state.finish()
+
